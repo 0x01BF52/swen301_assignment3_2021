b9fd85853eafd826d0b7112df6ee33c71d609005
0x01bf52 <25280745+0x01BF52@users.noreply.github.com>
2021-05-26 15:06:39 +1200

    refactor(Persistency and LogServlet): change Persistency's list type

--- src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/LogsServlet.java
@@ -1,12 +1,16 @@
 package nz.ac.wgtn.swen301.a3.server;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Enumeration;
+import java.util.Date;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * The type Logs servlet.
@@ -15,12 +19,18 @@ import java.util.Enumeration;
  * This class must have a default constructor (public, no parameters 1). [2 marks]
  */
 public class LogsServlet extends HttpServlet {
+    private final Persistency persistency = new Persistency();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+
+
     /**
      * Instantiates a new Logs servlet.
      */
-//    public LogsServlet() {
-//        //This class must have a default constructor (public, no parameters 1). [2 marks]
-//    }
+    public LogsServlet() {
+        //This class must have a default constructor (public, no parameters 1). [2 marks]
+    }
+
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("doDelete");
@@ -31,7 +41,6 @@ public class LogsServlet extends HttpServlet {
         System.out.println("doGet");
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-
         out.println("<html>");
         out.println("<head>");
         out.println("<title>Servlet</title>");
@@ -39,28 +48,40 @@ public class LogsServlet extends HttpServlet {
         out.println("<body>");
 
         out.println("<h1>Echoing Request Headers</h1>");
-        out.println("<table border><th>Header Name</th><th>Header Value</th>");
+        out.println("<table border><th>Index</th><th>Value</th>");
         // list headers send by the client
-        for (Enumeration headers = req.getHeaderNames(); headers.hasMoreElements();) {
-            String header = headers.nextElement().toString();
-            String value = req.getHeader(header);
+        var tempList = persistency.getList();
+        persistency.add(new LogEvent(UUID.randomUUID(),"random message",new Date(),"thread","logger", LevelEnum.ERROR, "string"));
+        for (var i = 0; i < tempList.size(); i++) {
+            var index = String.valueOf(i);
+            var value = tempList.get(i);
+
             out.print("<tr><td>");
-            out.print(header);
+            out.print(index);
             out.print("</td><td>");
-            out.print(value);
+            out.print(objectMapper.writeValueAsString(value));
             out.println("</td></tr>");
         }
-
+//        for (Enumeration headers = req.getHeaderNames(); headers.hasMoreElements(); ) {
+//            String header = headers.nextElement().toString();
+//            String value = req.getHeader(header);
+//            out.print("<tr><td>");
+//            out.print(header);
+//            out.print("</td><td>");
+//            out.print(value);
+//            out.println("</td></tr>");
+//        }
+//        req.getHeaderNames().asIterator().forEachRemaining(System.out::println);
         out.println("</table>");
         out.println("</body>");
         out.println("</html>");
-
         out.close();
-
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("doPost");
+        System.out.println("doPostInvoke");
+        var reqBody = req.getReader().lines().collect(Collectors.joining());
+        persistency.add(objectMapper.readValue(reqBody, LogEvent.class));
     }
 }
--- src/main/java/nz/ac/wgtn/swen301/a3/server/Persistency.java
+++ src/main/java/nz/ac/wgtn/swen301/a3/server/Persistency.java
@@ -1,17 +1,27 @@
 package nz.ac.wgtn.swen301.a3.server;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Persistency {
-    private List<String> list;
+    private final List<LogEvent> list;
 
     public Persistency() {
         this.list = new CopyOnWriteArrayList<>();
     }
 
-    public Boolean add(String s) {
+    public Boolean add(LogEvent s) {
         return this.list.add(s);
     }
 
+
+    public int getSize() {
+        return list.size();
+    }
+
+
+    public List<LogEvent> getList() {
+        return Collections.unmodifiableList(list);
+    }
 }
