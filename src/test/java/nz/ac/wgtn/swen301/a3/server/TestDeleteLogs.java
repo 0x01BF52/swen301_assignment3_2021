84bbc0a052322ddabd10419804b120a29530df09
0x01bf52 <25280745+0x01BF52@users.noreply.github.com>
2021-05-30 07:46:34 +1200

    style: typo

---
+++ src/test/java/nz/ac/wgtn/swen301/a3/server/TestDeleteLogs.java
@@ -0,0 +1,16 @@
+package nz.ac.wgtn.swen301.a3.server;
+
+import nz.ac.wgtn.swen301.a3.server.LogsServlet;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestDeleteLogs {
+
+}
